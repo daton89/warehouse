6c52aac694cc74b9a2df8eecdac2fddbe8802eb0
daton89 <tonydangelo123@gmail.com>
2017-06-16 02:23:02 +0200

    fixed types

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,51 +1,67 @@
+import { Article } from './article';
+import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import { Http } from "@angular/http";
-import { Article } from "app/article";
-import { Observable } from "rxjs/Observable";
+import { Subject } from "rxjs/Subject";
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+
+
 
 @Injectable()
 export class ArticleService {
 
   baseUri = 'http://localhost:9999/api/articles'
 
-  public collection: Observable<Article[]>
+  collection: Article[]
 
   constructor(
     private http: Http
-  ) {
+  ) {}
 
-  }
+  fetch(): Observable<Article[]> {
 
-  fetch() {
     return this.http.get(this.baseUri)
+      .map(articles => this.collection = articles.json() as Article[])
+
+    // .toPromise()
+    // .then(response => response.json().data as Article[])
+    // .catch(err => Promise.reject(err.message || err));
   }
 
-  getById(id) {
+  getById(id): Observable<Article[]> {
     return this.http.get(this.baseUri + '/' + id)
+      .map(articles => articles.json() as Article[])
   }
 
-  getByCode(code) {
+  getByCode(code): Observable<Article[]> {
     return this.http.get(this.baseUri + '/code/' + code)
+      .map(articles => articles.json() as Article[])
   }
 
-  getByName(name) {
+  getByName(name): Observable<Article[]> {
     return this.http.get(this.baseUri + '/name/' + name)
+      .map(articles => articles.json() as Article[])
   }
 
   create(article) {
 
-    // var { code, name, category, type, qty, company, price, nicotine, size } = article;
-
-    // let newArticle = new Article(
-    //   code, name, category, type, qty, company, price, nicotine, size
-    // )
-
     return this.http.post(this.baseUri, article)
+      .map(res => {
+
+        let article = res.json() as Article
+
+        this.collection.push(article)
+
+        return article // or this.collection as Article[]
+
+      })
 
   }
 
-  update(article) {
+  update(article): Observable<Article>{
     return this.http.put(this.baseUri + '/' + article._id, article)
+      .map(res => res.json() as Article)
   }
 
 }
