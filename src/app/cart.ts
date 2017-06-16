ce66de77940760f83d15074e1212cea32224dd37
daton89 <tonydangelo123@gmail.com>
2017-06-16 20:01:32 +0200

    add cart

---
+++ src/app/cart.ts
@@ -0,0 +1,12 @@
+
+export class Cart {
+
+  public _id: string
+  public products: Array<any>
+  public price: number
+  public createdOn: Date
+  public updatedOn: Date
+  public checkuot: boolean
+  public checkoutOn: Date
+
+}
