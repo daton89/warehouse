14b7fc432d3a0722713d42dc9153090b3c753304
daton89 <tonydangelo123@gmail.com>
2017-06-16 20:01:07 +0200

    update schema

--- server/api/cart/cart.model.js
+++ server/api/cart/cart.model.js
@@ -10,7 +10,9 @@ const cart = new mongoose.Schema({
     }],
     price: { type: Number, required: true },
     createdOn: { type: Date, default: Date.now() },
-    checkout: { type: Boolean, default: false }
+    updatedOn: { type: Date, default: Date.now() },
+    checkout: { type: Boolean, default: false },
+    checkoutOn: { type: Date, default: Date.now() }
 })
 
 module.exports = mongoose.model('Cart', cart)
