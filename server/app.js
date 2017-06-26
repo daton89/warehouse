6d8a39d93d611fdb5ac22c8a2c264f6c5f892d57
daton89 <tonydangelo123@gmail.com>
2017-06-27 00:46:15 +0200

    add config socketio

--- server/app.js
+++ server/app.js
@@ -14,25 +14,27 @@ const path = require('path');
 
 // Connect to MongoDB
 mongoose.connect(config.mongo.uri, config.mongo.options);
-mongoose.connection.on('error', function (err) {
-  console.error('MongoDB connection error: ' + err);
-  process.exit(-1);
+mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
 });
 
 // Populate databases with sample data
 if (config.seedDB) {
-  require('./config/seed');
+    require('./config/seed');
 }
 
 // Setup server
 var app = express();
 var server = http.createServer(app);
-// var socketio = require('socket.io').listen(server, {
-//     // serveClient: config.env !== 'production',
-//     serveClient: false,
-//     path: '/socket.io'
-// });
-// require('./config/socketio')(socketio);
+if (config.socketio) {
+    // var socketio = require('socket.io').listen(server, {
+    //     // serveClient: config.env !== 'production',
+    //     serveClient: false,
+    //     path: '/socket.io'
+    // });
+    // require('./config/socketio')(socketio);
+}
 
 require('./config/express')(app);
 
@@ -40,12 +42,12 @@ require('./routes')(app);
 
 // Start server
 function startServer() {
-  app.warehouse = server.listen(config.port, config.ip, function () {
-    console.log(`
+    app.warehouse = server.listen(config.port, config.ip, function() {
+        console.log(`
 
     `);
-    console.log('listening on %d, in %s mode on ip %s', config.port, app.get('env'), config.ip);
-  });
+        console.log('listening on %d, in %s mode on ip %s', config.port, app.get('env'), config.ip);
+    });
 }
 
 setImmediate(startServer);
