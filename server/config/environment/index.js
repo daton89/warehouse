f6d496471f3b0158207e2d97a2ec315594aafd4d
daton89 <tonydangelo123@gmail.com>
2017-08-15 00:56:31 +0200

    add mongo connection options

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -6,30 +6,39 @@ const path = require('path')
 console.log('Loading environment:', env.toUpperCase());
 
 const config = {
-  production: {
-    root: path.resolve(__dirname, '../../../'),
-    mongo: {
-      uri: 'mongodb://localhost/warehouse-production'
+    production: {
+        root: path.resolve(__dirname, '../../../'),
+        mongo: {
+            uri: 'mongodb://localhost/warehouse-production',
+            options: {
+                useMongoClient: true
+            }
+        },
+        port: process.env.PORT || 9999,
+        seedDB: false
     },
-    port: process.env.PORT || 9999,
-    seedDB: false
-  },
-  development: {
-    root: path.resolve(__dirname, '../../../'),
-    mongo: {
-      uri: 'mongodb://localhost/warehouse-development'
+    development: {
+        root: path.resolve(__dirname, '../../../'),
+        mongo: {
+            uri: 'mongodb://localhost/warehouse-development',
+            options: {
+                useMongoClient: true
+            }
+        },
+        port: process.env.PORT || 9999,
+        seedDB: false
     },
-    port: process.env.PORT || 9999,
-    seedDB: false
-  },
-  test: {
-    root: path.resolve(__dirname, '../../../'),
-    mongo: {
-      uri: 'mongodb://localhost/warehouse-test'
-    },
-    port: process.env.PORT || 9999,
-    seedDB: false
-  }
+    test: {
+        root: path.resolve(__dirname, '../../../'),
+        mongo: {
+            uri: 'mongodb://localhost/warehouse-test',
+            options: {
+                useMongoClient: true
+            }
+        },
+        port: process.env.PORT || 9999,
+        seedDB: false
+    }
 }
 
 module.exports = config[env];
