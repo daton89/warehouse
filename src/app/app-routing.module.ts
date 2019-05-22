8da01069ab64e5ae30b98f88869674736b893998
Toni D'Angelo <tonydangelo123@gmail.com>
2019-05-22 21:52:28 +0200

    refactor router module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AddArticleComponent } from './add-article/add-article.component';
-import { ArticleListComponent } from './article-list/article-list.component';
-import { ArticleImportComponent } from './article-import/article-import.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { AddArticleComponent } from './articles/add-article/add-article.component';
+import { ArticleListComponent } from './articles/article-list/article-list.component';
+import { ArticleImportComponent } from './articles/article-import/article-import.component';
 import { CartComponent } from './cart/cart.component';
+import { ArticleSearchComponent } from './articles/article-search/article-search.component';
+import { ArticleDetailComponent } from './articles/article-detail/article-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'articles', component: ArticleListComponent },
-  { path: 'articles/:id', component: AddArticleComponent },
+  { path: 'articles', component: ArticlesComponent, children: [
+    { path: '', component: ArticleSearchComponent},
+    { path: 'import', component: ArticleImportComponent },
+    { path: 'new', component: AddArticleComponent },
+    { path: ':id', component: ArticleDetailComponent },
+    { path: ':id/edit', component: AddArticleComponent },
+  ] },
   { path: 'cart', component: CartComponent },
-  { path: 'article-import', component: ArticleImportComponent }
 ];
 
 @NgModule({
