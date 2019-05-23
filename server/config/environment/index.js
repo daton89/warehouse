ed1066c01a83dc03e774a1fa903a8f0c3af1b6eb
Toni D'Angelo <tonydangelo123@gmail.com>
2019-05-23 12:17:47 +0200

    use mongo atlas db

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -1,44 +1,45 @@
-'use strict';
+"use strict";
 
 const env = process.env.NODE_ENV;
-const path = require('path')
+const path = require("path");
 
-console.log('Loading environment:', env.toUpperCase());
+console.log("Loading environment:", env.toUpperCase());
 
 const config = {
-    production: {
-        root: path.resolve(__dirname, '../../../'),
-        mongo: {
-            uri: 'mongodb://localhost/warehouse-production',
-            options: {
-                useMongoClient: true
-            }
-        },
-        port: process.env.PORT || 9999,
-        seedDB: false
+  production: {
+    root: path.resolve(__dirname, "../../../"),
+    mongo: {
+      uri: "mongodb://localhost/warehouse-production",
+      options: {
+        useMongoClient: true
+      }
     },
-    development: {
-        root: path.resolve(__dirname, '../../../'),
-        mongo: {
-            uri: 'mongodb://localhost/warehouse-development',
-            options: {
-                useMongoClient: true
-            }
-        },
-        port: process.env.PORT || 9999,
-        seedDB: false
+    port: process.env.PORT || 9999,
+    seedDB: false
+  },
+  development: {
+    root: path.resolve(__dirname, "../../../"),
+    mongo: {
+      uri:
+        "mongodb://warehouse:S9KwZIMoQFDebR21@daton-development-shard-00-00-nidnc.mongodb.net:27017,daton-development-shard-00-01-nidnc.mongodb.net:27017,daton-development-shard-00-02-nidnc.mongodb.net:27017/warehouse-development?ssl=true&replicaSet=daton-development-shard-0&authSource=admin&retryWrites=true",
+      options: {
+        useNewUrlParser: true
+      }
     },
-    test: {
-        root: path.resolve(__dirname, '../../../'),
-        mongo: {
-            uri: 'mongodb://localhost/warehouse-test',
-            options: {
-                useMongoClient: true
-            }
-        },
-        port: process.env.PORT || 9999,
-        seedDB: false
-    }
-}
+    port: process.env.PORT || 9999,
+    seedDB: false
+  },
+  test: {
+    root: path.resolve(__dirname, "../../../"),
+    mongo: {
+      uri: "mongodb://localhost/warehouse-test",
+      options: {
+        useMongoClient: true
+      }
+    },
+    port: process.env.PORT || 9999,
+    seedDB: false
+  }
+};
 
 module.exports = config[env];
