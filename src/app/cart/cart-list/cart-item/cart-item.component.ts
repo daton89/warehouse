b90eb9397b36e1020435209a36db884765bb4125
Tony D'Angelo <tonydangelo123@gmail.com>
2019-05-25 16:26:17 +0200

    implement setamount and remove cart item

--- src/app/cart/cart-list/cart-item/cart-item.component.ts
+++ src/app/cart/cart-list/cart-item/cart-item.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { map, debounceTime } from 'rxjs/operators';
+
+import { DataStorageService } from 'app/shared/data-storage.service';
 import { Product } from 'app/shared/product.model';
 
 @Component({
@@ -11,9 +14,28 @@ export class CartItemComponent implements OnInit {
   @Input() index: number
   @Input() even: boolean
   @Input() odd: boolean
+  @Input() cartId: string
+  numbers: Array<number> = [1,2,3,4,5]
 
-  constructor() { }
+  constructor(
+    private dataStorageService: DataStorageService
+  ) { }
 
   ngOnInit() { }
 
+  onSetAmount(product: Product, amountInput: HTMLInputElement) {
+    product.qty = +amountInput.value
+    this.dataStorageService.updateAmount(this.cartId, product)
+      .pipe(
+        map(() => alert(this.product.updated = true)),
+        debounceTime(3000)
+      )
+      .subscribe(
+        ()=> alert(this.product.updated = false)
+      )
+  }
+
+  onRemove(product: Product){
+    this.dataStorageService.removeProduct(this.cartId, product)
+  }
 }
