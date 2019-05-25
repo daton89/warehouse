db1d3c689f5c16ef97a9756fca6bdba5da0d752d
Tony D'Angelo <tonydangelo123@gmail.com>
2019-05-25 16:25:14 +0200

    add article and shopping list badges

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { CartService } from 'app/cart/cart.service';
+import { ArticleService, ArticleEvent } from 'app/articles/article.service';
+import { Cart } from 'app/cart/cart.model'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  articleSubscription: Subscription
+  cartSubscription: Subscription
+  articlesCount: number
+  cartProductsCount: number
 
-  constructor() { }
+  constructor(
+    private articleService: ArticleService,
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
+    this.articleSubscription = this.articleService.articlesChanged
+      .subscribe(
+        (event: ArticleEvent) => this.articlesCount = event.paginate.total
+      )
+    this.cartSubscription = this.cartService.cartChanged
+      .subscribe(
+        (cart: Cart) => this.cartProductsCount = cart.products.length
+      )
+  }
+
+  ngOnDestroy() {
+    this.articleSubscription.unsubscribe()
+    this.cartSubscription.unsubscribe()
   }
 
 }
