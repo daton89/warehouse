b82fa87c8fde8e68808ff37216230fa893a2209a
Tony D'Angelo <tonydangelo123@gmail.com>
2020-04-26 01:16:45 +0200

    remove inferred type

--- src/app/shared/image-preview.directive.ts
+++ src/app/shared/image-preview.directive.ts
@@ -4,7 +4,7 @@ import { Directive, HostBinding, HostListener } from '@angular/core';
   selector: '[appImagePreview]'
 })
 export class ImagePreviewDirective {
-  @HostBinding('style.max-height') maxHeight: string = '300px'
+  @HostBinding('style.max-height') maxHeight = '300px'
 
   @HostListener('mouseover') onHover() {
     this.maxHeight = '350px'
