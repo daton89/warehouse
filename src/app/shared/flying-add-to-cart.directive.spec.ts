32f76f319212ab89affc5cc5226e586cb79c4f8d
Tony D'Angelo <tonydangelo123@gmail.com>
2020-04-27 20:37:50 +0200

    generate flying add to cart directive

---
+++ src/app/shared/flying-add-to-cart.directive.spec.ts
@@ -0,0 +1,8 @@
+import { FlyingAddToCartDirective } from './flying-add-to-cart.directive'
+
+describe('FlyingAddToCartDirective', () => {
+  it('should create an instance', () => {
+    const directive = new FlyingAddToCartDirective()
+    expect(directive).toBeTruthy()
+  })
+})
---
+++ src/app/shared/flying-add-to-cart.directive.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core'
+
+@Directive({
+  selector: '[appFlyingAddToCart]',
+})
+export class FlyingAddToCartDirective {
+  constructor() {}
+}
