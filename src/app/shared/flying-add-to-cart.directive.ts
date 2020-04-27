d7729f299631948f6bca623ff9836eeec02fad71
Tony D'Angelo <tonydangelo123@gmail.com>
2020-04-27 22:00:06 +0200

    start implementation of FlyingDirective

--- src/app/shared/flying-add-to-cart.directive.ts
+++ src/app/shared/flying-add-to-cart.directive.ts
@@ -1,8 +1,22 @@
-import { Directive } from '@angular/core'
+import { Directive, HostBinding, HostListener, ElementRef, OnInit, ViewChild } from '@angular/core'
 
 @Directive({
   selector: '[appFlyingAddToCart]',
 })
-export class FlyingAddToCartDirective {
-  constructor() {}
+export class FlyingAddToCartDirective implements OnInit {
+  @HostBinding('class.flying') isFlying = false
+  @ViewChild('shoppingCartBadge') badge: ElementRef
+
+  constructor(private elmRef: ElementRef) {}
+
+  ngOnInit() {
+    console.log('elmRef', this.elmRef.nativeElement)
+    console.log('badge', this.badge.nativeElement)
+  }
+
+  @HostListener('click') toggleOpen() {
+    const { top } = this.elmRef.nativeElement.getBoundingClientRect()
+    console.log('top', top)
+    this.isFlying = !this.isFlying
+  }
 }
