d2e2b5adc73e4b173bd6a03ae80098b3a5e9b307
Tony D'Angelo <tonydangelo123@gmail.com>
2020-04-29 23:32:17 +0200

    add trasform

--- src/app/shared/flying-add-to-cart.directive.ts
+++ src/app/shared/flying-add-to-cart.directive.ts
@@ -1,22 +1,33 @@
-import { Directive, HostBinding, HostListener, ElementRef, OnInit, ViewChild } from '@angular/core'
+import { Directive, HostBinding, HostListener, ElementRef, OnInit, ViewChild, Renderer2 } from '@angular/core'
 
 @Directive({
   selector: '[appFlyingAddToCart]',
 })
 export class FlyingAddToCartDirective implements OnInit {
-  @HostBinding('class.flying') isFlying = false
-  @ViewChild('shoppingCartBadge') badge: ElementRef
+  // @HostBinding('class.flying') isFlying = false
 
-  constructor(private elmRef: ElementRef) {}
+  constructor(private elmRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
-    console.log('elmRef', this.elmRef.nativeElement)
-    console.log('badge', this.badge.nativeElement)
+    // console.log('elmRef', this.elmRef.nativeElement)
   }
 
   @HostListener('click') toggleOpen() {
-    const { top } = this.elmRef.nativeElement.getBoundingClientRect()
-    console.log('top', top)
-    this.isFlying = !this.isFlying
+    // this.isFlying = !this.isFlying
+
+    const badge = document.getElementById('shopping-cart-badge').getBoundingClientRect()
+
+    // transform: translate(350px,0);
+    // -webkit-transform: translate(350px,0); /** Chrome & Safari **/
+    // -o-transform: translate(350px,0); /** Opera **/
+    // -moz-transform: translate(350px,0); /** Firefox **/
+
+    this.renderer.setStyle(this.elmRef.nativeElement, 'transform', `translate(${badge.x}px, ${badge.y}px)`)
+    this.renderer.setStyle(this.elmRef.nativeElement, '-webkit-transform', `translate(${badge.x}px, ${badge.y}px)`)
+    this.renderer.setStyle(this.elmRef.nativeElement, '-o-transform', `translate(${badge.x}px, ${badge.y}px)`)
+    this.renderer.setStyle(this.elmRef.nativeElement, '-moz-transform', `translate(${badge.x}px, ${badge.y}px)`)
+
+    const elDomRect = this.elmRef.nativeElement.getBoundingClientRect()
+    console.log('elDomRect', elDomRect)
   }
 }
