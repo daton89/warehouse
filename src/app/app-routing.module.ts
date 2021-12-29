7e3bb0f82bdd99e095f47eb51ddd7eff7a37c593
Toni D'Angelo <tonydangelo123@gmail.com>
2021-12-29 23:40:41 +0100

    upgrade to 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,33 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ArticlesComponent } from './articles/articles.component';
-import { AddArticleComponent } from './articles/add-article/add-article.component';
-import { ArticleImportComponent } from './articles/article-import/article-import.component';
-import { CartComponent } from './cart/cart.component';
-import { ArticleSearchComponent } from './articles/article-search/article-search.component';
-import { ArticleDetailComponent } from './articles/article-detail/article-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component'
+import { ArticlesComponent } from './articles/articles.component'
+import { AddArticleComponent } from './articles/add-article/add-article.component'
+import { ArticleImportComponent } from './articles/article-import/article-import.component'
+import { CartComponent } from './cart/cart.component'
+import { ArticleSearchComponent } from './articles/article-search/article-search.component'
+import { ArticleDetailComponent } from './articles/article-detail/article-detail.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'articles', component: ArticlesComponent, children: [
-    { path: '', component: ArticleSearchComponent},
-    { path: 'import', component: ArticleImportComponent },
-    { path: 'new', component: AddArticleComponent },
-    { path: ':id', component: ArticleDetailComponent },
-    { path: ':id/edit', component: AddArticleComponent },
-  ] },
+  {
+    path: 'articles',
+    component: ArticlesComponent,
+    children: [
+      { path: '', component: ArticleSearchComponent },
+      { path: 'import', component: ArticleImportComponent },
+      { path: 'new', component: AddArticleComponent },
+      { path: ':id', component: ArticleDetailComponent },
+      { path: ':id/edit', component: AddArticleComponent },
+    ],
+  },
   { path: 'cart', component: CartComponent },
-];
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/articles/article-import/article-import.component.spec.ts
+++ src/app/articles/article-import/article-import.component.spec.ts
@@ -1,25 +1,26 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing'
 
-import { ArticleImportComponent } from './article-import.component';
+import { ArticleImportComponent } from './article-import.component'
 
 describe('ArticleImportComponent', () => {
-  let component: ArticleImportComponent;
-  let fixture: ComponentFixture<ArticleImportComponent>;
+  let component: ArticleImportComponent
+  let fixture: ComponentFixture<ArticleImportComponent>
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ArticleImportComponent ]
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ArticleImportComponent],
+      }).compileComponents()
     })
-    .compileComponents();
-  }));
+  )
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ArticleImportComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(ArticleImportComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
